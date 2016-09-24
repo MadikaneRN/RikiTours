1c45be0b15cd12ed7dd7b61d968d917c2c0831aa
berkaayowa <ayowaberka@gmail.com>
2016-09-24 02:13:41 +0200

    added packages and error interface

---
+++ RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/error/Error.java
@@ -0,0 +1,8 @@
+package za.co.whcb.tp2.rikitours.error;
+
+/**
+ * Created by berka on 9/23/2016.
+ */
+public interface Error {
+    String getError(int index);
+}
---
+++ RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/processingmessage/Display.java
@@ -0,0 +1,7 @@
+package za.co.whcb.tp2.rikitours.processingmessage;
+
+/**
+ * Created by kelly on 9/23/2016.
+ */
+public class Display {
+}
