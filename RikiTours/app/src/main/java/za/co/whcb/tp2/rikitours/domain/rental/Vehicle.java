ce937c8f76f727646eea6fa239e45e9d2bffd23a
MadikaneRN <richardmadikane100@gmail.com>
2016-10-03 13:49:08 +0200

    temp

---
+++ RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/domain/rental/Vehicle.java
@@ -0,0 +1,85 @@
+package za.co.whcb.tp2.rikitours.domain.rental;
+
+import java.io.Serializable;
+
+/**
+ * Created by Scorpian on 2016-10-03.
+ */
+
+public class Vehicle implements Serializable{
+
+    private long id;
+    private String vehicleName;
+    private String vehicleModel;
+    private String vehicleYear;
+
+
+    public Vehicle() {
+    }
+
+    public long getIdNumber() {
+        return id;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public String getVehicleYear() {
+        return vehicleYear;
+    }
+
+
+    public static class Builder{
+        private Long id;
+        private String vehicleName;
+        private String vehicleModel;
+        private  String vehicleYear;
+
+        public Builder id(Long value){
+            this.id = value;
+            return this;
+        }
+
+        public Builder vehicleName(String value){
+            this.vehicleName = value;
+            return this;
+        }
+
+        public Builder vehicleModel(String value){
+            this.vehicleModel = value;
+            return this;
+        }
+
+        public Builder vehicleYear(String value){
+            this.vehicleYear = value;
+            return this;
+        }
+
+        public Builder copy(Vehicle value){
+            this.id = value.id;
+            this.vehicleName = value.vehicleName;
+            this.vehicleModel = value.vehicleModel;
+            this.vehicleYear = value.vehicleYear;
+            return this;
+        }
+
+        public Vehicle build(){return new Vehicle(this);}
+    }
+
+    public Vehicle(Builder builder){
+        this.id = builder.id;
+        this.vehicleName = builder.vehicleName;
+        this.vehicleModel = builder.vehicleModel;
+        this.vehicleYear = builder.vehicleYear;
+    }
+
+
+
+
+
+}
