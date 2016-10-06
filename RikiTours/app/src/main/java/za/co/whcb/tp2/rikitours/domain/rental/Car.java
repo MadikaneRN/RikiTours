a35118c217edbd8a31db15c37fd5d6db22ccf10a
MadikaneRN <richardmadikane100@gmail.com>
2016-10-06 09:40:10 +0200

    Updated Car

--- RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/domain/rental/Car.java
+++ RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/domain/rental/Car.java
@@ -3,66 +3,53 @@ package za.co.whcb.tp2.rikitours.domain.rental;
 /**
  * Created by berka on 9/17/2016.
  */
-public class Car {
-    private String name, type, color, id;
-    private int numberOfSeat, year;
+public class Car implements Serializable{
+    
+    private long id;
+    private String regNumber;
 
     public Car() {
     }
-
-    public Car(String name, String type, int numberOfSeat, int year, String color) {
-        this.name = name;
-        this.type = type;
-        this.numberOfSeat = numberOfSeat;
-        this.year = year;
-        this.color = color;
-    }
-
+    
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getRegistrationNumber()
+    {
+        return reGNumber;
+    }
+    
+    public Car(Builder builder) {
+
+        this.id = builder.id;
+        this.regNumber = builder.regNumber;
+        
+    }
+    
+    public static class Builder{
+        private long id;
+        private String regNumber;
+
+        public Builder id(long value){
+            this.id = value;
+            return this;
+        }
+        public Builder regNumber(String value){
+            this.regNumber = value;
+            return this;
+        }
+      
+
+        public Builder copy(SUV value){
+            this.id = value.id;
+            this.regNumber = value.regNumber;
+            return this;
+        }
+
+        public Car build(){return new Car(this);}
     }
 
-    public String getName() {
-        return name;
-    }
+    
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getNumberOfSeat() {
-        return numberOfSeat;
-    }
-
-    public void setNumberOfSeat(int numberOfSeat) {
-        this.numberOfSeat = numberOfSeat;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
