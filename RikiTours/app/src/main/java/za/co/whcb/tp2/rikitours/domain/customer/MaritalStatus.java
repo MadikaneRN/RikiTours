80cee0b561c06840480887d5762f1fe4d3c144c4
tamonne <tamonnecotodie@gmail.com>
2016-10-09 05:07:32 +0200

    Addd Customer Domain Classes

---
+++ RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/domain/customer/MaritalStatus.java
@@ -0,0 +1,92 @@
+package za.co.whcb.tp2.rikitours.domain.customer;
+
+/**
+ * Created by Tamonne on 2016/04/16.
+ */
+public class MaritalStatus implements Serializable {
+
+    private Long id;
+    private String customerId;
+    private String single;
+    private string married;
+    private String divorced;
+
+
+    public MaritalStatus(Builder buildObj){
+	customerId=buildObj.customerId;
+        single=buildObj.single;
+        married=buildObj.married;
+        divorced=buildObj.divorced;
+        id=buildObj.id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIsSingle() {
+        return single;
+    }
+
+    public String getIsMarried() {
+        return married;
+    }
+	
+	public String getCustomeId() {
+        return customerId;
+    }
+
+    public String getIsDivorced() {
+        return divorced;
+    }
+
+
+    private MaritalStatus(){}
+
+    public static class Builder {
+        private Long id;
+     private String customerId;
+        private String single;
+        private string married;
+        private String divorced;
+
+ public Builder customerId (String customerId){
+        this.customerId=customerId;
+            return this;
+
+        }
+        public Builder single (String single){
+        this.single=single;
+            return this;
+
+        }
+        public Builder married (String married){
+        this.married=married;
+            return this;
+
+        }
+        public Builder id (Long id){
+            this.id=id;
+            return this;
+
+        }
+
+        public Builder divorced(String divorced){
+            this.divorced=divorced;
+            return this;
+        }
+    public Builder copyObj(MaritalStatus maritalStatus){
+        this.customerId=emergencyContact.getcustomerId();
+        this.married=maritalStatus.getIsMarried();
+        this.divorced=maritalStatus.getIsDivorced();
+        this.single=maritalStatus.getisSingle();
+        this.id=maritalStatus.getId();
+        return this;
+    }
+        public MaritalStatus build (){
+            return new MaritalStatus(this);
+        }
+    }
+
+
+}
