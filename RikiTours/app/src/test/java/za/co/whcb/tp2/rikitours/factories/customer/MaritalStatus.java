90b2d44c02ed24235f222a5986153367ce2ae627
Inno - The tech geek <Inno - The tech geek>
2016-10-10 16:17:24 +0200

    Completed test cases for the Customer Domain

---
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/MaritalStatus.java
@@ -0,0 +1,15 @@
+package za.co.whcb.tp2.rikitours.factories.customer;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import za.co.whcb.tp2.rikitours.domain.customer.EmergencyContact;
+
+/**
+ * Created by Game330 on 2016-10-10.
+ */
+public class MaritalStatus {
+
+
+}
