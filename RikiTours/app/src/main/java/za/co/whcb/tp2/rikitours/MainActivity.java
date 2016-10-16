e78dd2f0aba6e11a501c5eeca26009767b881f1c
MadikaneRN <richardmadikane100@gmail.com>
2016-10-16 19:15:22 +0200

    [CL-RK-2] completed Driver CRUD and service

--- RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/MainActivity.java
+++ RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/MainActivity.java
@@ -8,14 +8,19 @@ import android.os.Bundle;
 
 
 import za.co.whcb.tp2.rikitours.common.Display;
+import za.co.whcb.tp2.rikitours.domain.rental.Driver;
 import za.co.whcb.tp2.rikitours.domain.rental.Vehicle;
 
+import za.co.whcb.tp2.rikitours.factories.rental.DriverFactory;
 import za.co.whcb.tp2.rikitours.factories.rental.VehicleFactory;
 
+import za.co.whcb.tp2.rikitours.repository.local.rental.DriverRepo;
 import za.co.whcb.tp2.rikitours.repository.local.rental.VehicleRepo;
 
 public class MainActivity extends AppCompatActivity {
     private VehicleRepo vehicleRepo;
+
+    private DriverRepo driverRepo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +28,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        vehicleRepo = new VehicleRepo(this);
+        //vehicleRepo = new VehicleRepo(this);
         //UpdateRepo
         //Vehicle vehicle = VehicleFactory.getVehicle(12l,"AUDI","M3","2014");
 
+        driverRepo = new DriverRepo(this);
+
+        //Driver driver = DriverFactory.getDriver(10,"Madikane","CA33","yes");
+
+        Driver driver = driverRepo.findDriverById(10);
+        Display.toast(driver.getFname(),this);
+
+        /*if(driverRepo.addDriver(driver) == true)
+        {
+            Display.toast(driver.getFname(),this);
+        }
+        else
+        {
+            Display.toast("failed to add Driver",this);
+        }
+*/
 
         //vehicleRepo = new VehicleRepo(this);
         //Vehicle vehicle = VehicleFactory.getVehicle(21,"BMW","M3","2013");
 
-      Vehicle vehicle = vehicleRepo.findVehicleById(12l);
+      //Vehicle vehicle = vehicleRepo.findVehicleById(12l);
 
-        Display.toast(vehicle.getVehicleName(), this);
+       // Display.toast(vehicle.getVehicleName(), this);
+
+
 
        /* if (vehicleRepo.addVehicle(vehicle)== true) {
             Display.toast("Vehicle added ", this);
