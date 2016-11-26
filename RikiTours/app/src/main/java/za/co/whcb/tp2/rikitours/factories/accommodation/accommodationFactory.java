2df0ee0f3607a41ec3da80c46af7b40c9878c3b2
MadikaneRN <richardmadikane@gmail.com>
2016-11-26 15:37:34 +0200

    Kiershans Package Created Hotel Test and PassedADP-RK-1]

--- RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/factories/accommodation/accommodationFactory.java
+++ RikiTours/app/src/main/java/za/co/whcb/tp2/rikitours/factories/accommodation/accommodationFactory.java
@@ -1,7 +1,11 @@
 package za.co.whcb.tp2.rikitours.factories.accommodation;
 
+import za.co.whcb.tp2.rikitours.domain.accommodation.Hotel;
+
 /**
  * Created by berka on 11/26/2016.
  */
-public class accommodationFactory {
+public class AccommodationFactory {
+
+
 }
