a3bd6c31ef2b36c7b38b6e8f3476f4a6ded41588
tamonne <tamonnecotodie@gmail.com>
2016-11-30 16:46:16 +0200

    Added Customer Test

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/CustomerTest.java
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/CustomerTest.java
@@ -1,38 +1,28 @@
 package za.co.whcb.tp2.rikitours.factories.customer;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
 import za.co.whcb.tp2.rikitours.domain.customer.Customer;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 /**
- * Created by Game330 on 2016-10-10.
+ * Created by Tamonne on 11/27/2016.
  */
 public class CustomerTest {
+    @Test
+    public void testCreate() throws Exception {
+        Customer customer=CustomerFactory.getCustomer(2,"Tamie","Cotodie","7709");
+        Assert.assertNotNull(customer);
+        Assert.assertEquals(2,customer.getId());
+        Assert.assertEquals("Cotodie",customer.getSurname());
 
-    private Customer cust;
 
-   /* @Before
-    public void setUp() throws Exception {
-        cust= new CustomerFactory().getCustomer("Tam","cotodie","55");
+
     }
 
 
-    @Test
-    public void testCustomer() throws Exception {
-        Assert.assertEquals(cust.getName(),"Tam");
-//        Assert.assertEquals(cust.getCustomerNumber(),"55");
 
-    }
 
-    @Test
-    public void testUpdate() throws Exception {
-        Customer upCust=new Customer.Builder().name("thamar").surname("johns").customer_no("89").build();
-        Assert.assertEquals(upCust.getSurname(),"johns");
-//        Assert.assertEquals(upCust.getCustomerNumber(),"89");
 
-    }*/
+
 }
