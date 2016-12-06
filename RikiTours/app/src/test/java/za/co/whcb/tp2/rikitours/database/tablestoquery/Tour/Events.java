ad8f7ffb0fb7b4d337fedd95871a2caae56511ca
tamonne <tamonnecotodie@gmail.com>
2016-12-06 10:33:29 +0200

    Refactoring

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/database/tablestoquery/Tour/Events.java
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/database/tablestoquery/Tour/Events.java
@@ -9,6 +9,7 @@ import za.co.whcb.tp2.rikitours.config.database.table.tour.EventsTable;
  * Created by work on 10/10/2016.
  */
 public class Events {
+
     @Test
     public void testEvents() throws Exception {
 
