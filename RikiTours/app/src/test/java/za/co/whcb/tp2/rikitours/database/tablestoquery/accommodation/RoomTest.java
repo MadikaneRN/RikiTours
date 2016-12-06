dd1be49d398c1beecda9dd2c5c4e49b125169467
LindorMabuya <lindormabuya@gmail.com>
2016-12-06 22:45:01 +0200

    acc package updates

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/database/tablestoquery/accommodation/RoomTest.java
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/database/tablestoquery/accommodation/RoomTest.java
@@ -1,9 +1,20 @@
 package za.co.whcb.tp2.rikitours.database.tablestoquery.accommodation;
 
 
+import org.junit.Test;
+
+import za.co.whcb.tp2.rikitours.common.Converter;
+import za.co.whcb.tp2.rikitours.config.database.table.accommodation.RoomTable;
+
 /**
  * Created by lindo on 2016-12-03.
  */
 public class RoomTest {
+    @Test
+    public void testRoomTable() throws Exception {
+        RoomTable roomTable = new RoomTable();
+        String query = Converter.toCreateTableQuery(roomTable.getTableName(),roomTable.getAllAttributes());
+        System.out.println(query);
 
+    }
 }
