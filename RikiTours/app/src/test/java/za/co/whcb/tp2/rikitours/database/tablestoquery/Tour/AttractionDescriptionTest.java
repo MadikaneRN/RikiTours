2a71b76c9c44e01f179104a8a2e3868f1f66582b
tamonne <tamonnecotodie@gmail.com>
2016-12-06 10:11:12 +0200

    Refactoring

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/database/tablestoquery/Tour/AttractionDescriptionTest.java
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/database/tablestoquery/Tour/AttractionDescriptionTest.java
@@ -9,6 +9,7 @@ import za.co.whcb.tp2.rikitours.config.database.table.tour.AttractionDescription
  * Created by work on 10/10/2016.
  */
 public class AttractionDescriptionTest {
+    
     @Test
     public void testAttractionDescriptionTable() throws Exception {
 
