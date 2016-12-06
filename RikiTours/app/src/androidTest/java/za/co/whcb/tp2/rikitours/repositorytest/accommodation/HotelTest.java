36083f5a1c192b84b01d435e0f9920b675cf532c
LindorMabuya <lindormabuya@gmail.com>
2016-12-06 22:50:22 +0200

    hotel repo test added

--- RikiTours/app/src/androidTest/java/za/co/whcb/tp2/rikitours/repositorytest/accommodation/HotelTest.java
+++ RikiTours/app/src/androidTest/java/za/co/whcb/tp2/rikitours/repositorytest/accommodation/HotelTest.java
@@ -1,7 +1,28 @@
 package za.co.whcb.tp2.rikitours.repositorytest.accommodation;
 
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import za.co.whcb.tp2.rikitours.repository.local.accommodation.HotelRepo;
+
 /**
  * Created by bishop v on 2016-12-06.
  */
-public class HotelTest {
+public class HotelTest extends AndroidTestCase {
+    private HotelRepo hotelRepo;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+        hotelRepo = new HotelRepo(context);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        hotelRepo.close();
+        super.tearDown();
+    }
+
+    
 }
