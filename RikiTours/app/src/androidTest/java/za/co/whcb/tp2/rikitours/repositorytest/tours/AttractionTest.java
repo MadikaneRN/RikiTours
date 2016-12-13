00d60a60bb4a38ecadb146261f62a0a51fbf5f97
tamonne <tamonnecotodie@gmail.com>
2016-12-13 13:27:21 +0200

    refactoring

--- RikiTours/app/src/androidTest/java/za/co/whcb/tp2/rikitours/repositorytest/tours/AttractionTest.java
+++ RikiTours/app/src/androidTest/java/za/co/whcb/tp2/rikitours/repositorytest/tours/AttractionTest.java
@@ -15,10 +15,12 @@ import za.co.whcb.tp2.rikitours.repository.local.tour.AttractionsRepo;
  * Created by Shaun Mesias on 2016/10/17.
  */
 public class AttractionTest extends AndroidTestCase {
+
     private AttractionsRepo attractionRepo ;
 
     @Override
     public void setUp() throws Exception {
+
         super.setUp();
         RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
         attractionRepo = new AttractionsRepo(context);
@@ -26,20 +28,25 @@ public class AttractionTest extends AndroidTestCase {
 
     @Override
     public void tearDown() throws Exception {
+
         attractionRepo.close();
         super.tearDown();
     }
 
     public void addAttractionDescriptionTest() {
+
         AttractionDescription description = AttractionDescriptionFactory.getAttractionDescription(1L,"one", "two", "three", "four");
         Country country = CountryFactory.getCountry(1L, "south-africa", "South Africa is a country on the southernmost", "sa.jpg");
+
         Attraction attraction = AttractionFactory.getAttraction(1L, country, description);
         boolean isAdded = attractionRepo.addAttraction(attraction);
+
         assertNull(description);
         assertTrue(isAdded);
     }
 
     public void findAttractionDescriptionById() {
+
         Attraction description = attractionRepo.findAtractionById(1L);
         assertNull(description);
     }
