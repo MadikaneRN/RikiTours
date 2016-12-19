aba2b53a71b4cda9652c57a7b10c777f98d12647
tamonne <tamonnecotodie@gmail.com>
2016-12-19 09:36:56 +0200

    Added Occupational Details Test

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/OccupationalDetailsTest.java
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/OccupationalDetailsTest.java
@@ -1,7 +1,16 @@
 package za.co.whcb.tp2.rikitours.factories.customer;
 
+import org.junit.Test;
+
 /**
  * Created by Tamonne on 12/19/2016.
  */
 public class OccupationalDetailsTest {
+    @Test
+    public void testCreate() throws Exception {
+        
+    }
+
+
+
 }
