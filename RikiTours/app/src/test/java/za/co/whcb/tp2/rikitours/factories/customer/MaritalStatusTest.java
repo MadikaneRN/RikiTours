3bfd42c8d8075196122e110b1442797a9309858c
tamonne <tamonnecotodie@gmail.com>
2016-12-19 09:31:43 +0200

    added MaritalStatus Test

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/MaritalStatusTest.java
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/MaritalStatusTest.java
@@ -1,8 +1,20 @@
 package za.co.whcb.tp2.rikitours.factories.customer;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import za.co.whcb.tp2.rikitours.domain.customer.MaritalStatus;
+
 /**
  * Created by Tamonne on 12/19/2016.
  */
 public class MaritalStatusTest {
+    @Test
+    public void testCreate() throws Exception {
+
+      
+
+
+    }
 
 }
