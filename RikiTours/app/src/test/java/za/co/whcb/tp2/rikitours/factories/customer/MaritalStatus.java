d39e6b20b6ba3779776a02798143f7748a608234
tamonne <tamonnecotodie@gmail.com>
2016-12-19 09:29:25 +0200

    refactoring MaritalStatus test in factory

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/MaritalStatus.java
+++
@@ -1,9 +0,0 @@
-package za.co.whcb.tp2.rikitours.factories.customer;
-
-/**
- * Created by Game330 on 2016-10-10.
- */
-public class MaritalStatus {
-
-
-}
---
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/MaritalStatusTest.java
@@ -0,0 +1,8 @@
+package za.co.whcb.tp2.rikitours.factories.customer;
+
+/**
+ * Created by Tamonne on 12/19/2016.
+ */
+public class MaritalStatusTest {
+
+}
