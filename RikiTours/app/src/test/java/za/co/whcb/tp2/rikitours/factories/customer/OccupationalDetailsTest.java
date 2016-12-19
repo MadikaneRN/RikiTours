7a30f4f05cfbaefbde59964acc148d3cda035f1f
tamonne <tamonnecotodie@gmail.com>
2016-12-19 09:40:07 +0200

    Added Occupational Details Test

--- RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/OccupationalDetailsTest.java
+++ RikiTours/app/src/test/java/za/co/whcb/tp2/rikitours/factories/customer/OccupationalDetailsTest.java
@@ -1,6 +1,9 @@
 package za.co.whcb.tp2.rikitours.factories.customer;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import za.co.whcb.tp2.rikitours.domain.customer.OccupationalDetails;
 
 /**
  * Created by Tamonne on 12/19/2016.
@@ -8,7 +11,15 @@ import org.junit.Test;
 public class OccupationalDetailsTest {
     @Test
     public void testCreate() throws Exception {
-        
+        OccupationalDetails occupationalDetails= OccupationDetailsFactory.getContact("2","Clerk","KC","Admin");
+        Assert.assertNotNull(occupationalDetails);
+        Assert.assertEquals("2",occupationalDetails.getCustomerId());
+        Assert.assertEquals("KC",occupationalDetails.getCompanyName());
+
+
+
+
+
     }
 
 
